63de545495ad280e824a87797f3a0e67cd9756e2
Mujtaba Mubasher <81616586+MujtabaMubasher@users.noreply.github.com>
2025-03-03 14:24:16 +0500

    Update app.js

--- src/app.js
+++ src/app.js
@@ -1,6 +1,9 @@
 import express from "express"
 import cookieParser from "cookie-parser";
 import cors from 'cors'
+import dotenv from "dotenv";
+
+dotenv.config();
 
 
 const app = express()
@@ -10,7 +13,7 @@ app.use(cors({
     credentials: true,
 }))
 
-app.use(cookieParser())
+app.use(cookieParser(process.env.ACCESS_TOKEN_SECRET))
 app.use(express.json({limit: "16kb"}))
 app.use(express.urlencoded({extended:true, limit:"16kb"}))
 app.use(express.static('public'))
